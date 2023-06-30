df8d11650e99a19de9d885b3e045555c539eca4e
Ruben Ramirez <rramirez110911@gmail.com>
2023-06-29 23:21:15 -0700

    changes made

---
+++ front-end/profile-portal/src/Components/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <form>
+      <label htmlFor=''>email</label>
+      <input type='text' />
+      <label htmlFor=''>password</label>
+      <input type='text' />
+      <button>Log In</button>
+      <p>
+        No account? <span>CREATE ONE</span>
+      </p>
+    </form>
+  )
+}
+
+export default Form
--- front-end/profile-portal/src/Pages/Home.jsx
+++ front-end/profile-portal/src/Pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Form from '../Components/Form'
 
 function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <Form />
+    </div>
+  )
 }
 
 export default Home
