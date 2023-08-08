e54ae3b60e8f555499bdb61476ab57ca339cd711
Ruben Ramirez <rramirez110911@gmail.com>
2023-08-07 22:29:49 -0700

    creates sign up form

---
+++ front-end/profile-portal/src/Components/SignUpForm.jsx
@@ -0,0 +1,11 @@
+function SignUpForm() {
+  return (
+    <form>
+      <input type='text' name='' id='' />
+      <input type='text' name='' id='' />
+      <input type='' name='' id='' />
+    </form>
+  )
+}
+
+export default SignUpForm
