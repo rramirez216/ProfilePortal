0be1e38b74b9424e4a59d54f141476292ba26c56
Ruben Ramirez <rramirez110911@gmail.com>
2023-08-15 21:14:49 -0700

    adds state

--- front-end/profile-portal/src/Pages/Home.jsx
+++ front-end/profile-portal/src/Pages/Home.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignUpForm from '../Components/SignUpForm'
 
 function Home() {
+  const [firstName, setFirstName] = useState('ruben')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [formData, setFormData] = useState({})
+
   return (
     <div>
       <h2>Create User</h2>
-      <SignUpForm />
+      <SignUpForm
+        setFirstName={setFirstName}
+        firstName={firstName}
+        setLastName={setLastName}
+        lastName={lastName}
+        setEmail={setEmail}
+        email={email}
+        setPassword={setPassword}
+        password={password}
+        setFormData={setFormData}
+        formData={formData}
+      />
     </div>
   )
 }
