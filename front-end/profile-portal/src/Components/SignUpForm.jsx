3482a0dc3ad1c85d1dd733b2a82599fde7bdd24a
Ruben Ramirez <rramirez110911@gmail.com>
2023-08-22 21:35:46 -0700

    passes props

--- front-end/profile-portal/src/Components/SignUpForm.jsx
+++ front-end/profile-portal/src/Components/SignUpForm.jsx
@@ -1,16 +1,51 @@
+/* eslint-disable react/prop-types */
 import styled from 'styled-components'
 
-function SignUpForm(props) {
+function SignUpForm({
+  firstName,
+  setFirstName,
+  lastName,
+  setLastName,
+  email,
+  setEmail,
+  password,
+  setPassword,
+  handleSubmit,
+}) {
   return (
-    <form>
-      <Label htmlFor='first_name'>First Name:</Label>
-      <Input type='text' id='first-name' value={props.firstName} />
-      <Label htmlFor='last_name'>Last Name:</Label>
-      <Input type='text' id='last-name' />
+    <form onSubmit={handleSubmit}>
+      <Label htmlFor='first-name'>First Name:</Label>
+      <Input
+        type='text'
+        id='first-name'
+        name='first-name'
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <Label htmlFor='last-name'>Last Name:</Label>
+      <Input
+        type='text'
+        id='last-name'
+        name='last-name'
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
       <Label htmlFor='email'>Email:</Label>
-      <Input type='email' id='email' />
+      <Input
+        type='email'
+        id='email'
+        name='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <Label htmlFor='password'>Password:</Label>
-      <Input type='password' id='password' />
+      <Input
+        type='password'
+        id='password'
+        name='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Create</button>
     </form>
   )
