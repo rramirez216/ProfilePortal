aaaabacc8610fa64c939a0f557cfa8be2fb436f9
Ruben Ramirez <rramirez110911@gmail.com>
2023-09-11 19:59:02 -0700

    conditionally renders a list item

--- front-end/profile-portal/src/Components/List.jsx
+++ front-end/profile-portal/src/Components/List.jsx
@@ -1,5 +1,13 @@
-function List() {
-  return <div>List</div>
+function List({ userList }) {
+  return (
+    <div>
+      {userList ? (
+        <div>{userList[0].first_name}</div>
+      ) : (
+        <div>names go here</div>
+      )}
+    </div>
+  )
 }
 
 export default List
